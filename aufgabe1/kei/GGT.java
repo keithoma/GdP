0a6c7c8df27bac94a21ca01c66d2439eb6c65024
Christian Parpart <christian@parpart.family>
2019-11-03 23:12:50 +0100

    updates, zweite version von GGT.java (fuer Kei)

--- aufgabe1/kei/GGT.java
+++ aufgabe1/kei/GGT.java
@@ -1,7 +1,10 @@
-// Praktikumsaufgabe
-public class GGT {
-    public static int ggt(int a, int b) {
-        while (b != 0) {
+// Praktikum, Aufgabe 1
+public class GGT
+{
+    public static int ggt(int a, int b)
+    {
+        while (b != 0)
+        {
             int m = a % b;
             a = b;
             b = m;
@@ -11,16 +14,21 @@ public class GGT {
 
     public static void main(String[] args)
     {
-        if (args.length != 2) {
-            System.out.println("Usage: GGT <a> <b>\n");
+        if (args.length != 2)
+        {
+            System.out.println("Benutz mich: GGT a b            | a > 0 ^ b > 0\n");
             System.exit(1);
         }
+
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        if (a <= 0 || b <= 0) {
-            System.out.println("Both parameters must be natural numbers, greater than zero.");
+
+        if (a <= 0 || b <= 0)
+        {
+            System.out.println("Beide Parameter muessen groesser als 0 sein.");
             System.exit(2);
         }
+
         System.out.printf("ggT(%d, %d) = %d\n", a, b, ggt(a, b));
     }
 }
