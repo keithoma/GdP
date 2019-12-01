84e91232f646dd25ae327afdc0ad404eb0d35e22
Christian Parpart <christian@parpart.family>
2019-12-01 23:30:31 +0100

    fixes and tidyness.

--- 05week/PrimeUtils.java
+++ 05week/PrimeUtils.java
@@ -1,40 +1,17 @@
 class PrimeUtils {
-    // tod the trim thing could be a function
-
-
-    public static boolean isPrime(short n) {
-        // check naively from 2 to sqrt(n) if it is divisible by a positive integer
-        double sqrt = Math.sqrt(n);
-        for (double i=2.0; i < sqrt; ++i) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean isPrime(int n) {
-        // check naively from 2 to sqrt(n) if it is divisible by a positive integer
-        double sqrt = Math.sqrt(n);
-        for (double i=2.0; i < sqrt; ++i) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static boolean isPrime(long n) {
         // check naively from 2 to sqrt(n) if it is divisible by a positive integer
         double sqrt = Math.sqrt(n);
-        for (double i=2.0; i < sqrt; ++i) {
-            if (n % i == 0) {
+        for (double i = 2.0; i < sqrt; ++i)
+            if (n % i == 0)
                 return false;
-            }
-        }
+
         return true;
     }
-    
+
+    public static boolean isPrime(int n) { return isPrime((long) n); }
+    public static boolean isPrime(short n) { return isPrime((long) n); }
+
     public static int[] getFirstPrimes(int upto) {
         boolean number_line[] = new boolean[upto + 1];
         int primes[] = new int[upto + 1];
@@ -45,7 +22,7 @@ class PrimeUtils {
         int i = 0;
         for (int p = 2; p <= upto; ++p) 
         {
-            if (number_line[p] == true)
+            if (number_line[p])
             {
                 primes[i] = p;
                 ++i;
@@ -55,11 +32,9 @@ class PrimeUtils {
         }
 
         int primes_trimmed[] = new int[i];
-        for (int j = 0; j < i; ++j) {
-            if (primes[j] != 0) {
+        for (int j = 0; j < i; ++j)
+            if (primes[j] != 0)
                 primes_trimmed[j] = primes[j];
-            }
-        }
 
         return primes_trimmed;
     }
@@ -76,26 +51,30 @@ class PrimeUtils {
         }
 
         int primes_between_trimmed[] = new int[counter];
-        for (int j = 0; j < counter; ++j) {
-            if (primes_between[j] != 0) {
+        for (int j = 0; j < counter; ++j)
+            if (primes_between[j] != 0)
                 primes_between_trimmed[j] = primes_between[j];
-            }
-        }
 
         return primes_between_trimmed;
     }
 
     public static boolean isContained(int value, int[] array) {
-        for (int i = 0; i < array.length; ++i) {
-            if (value == array[i]) {
+        int a = 0;
+        int b = array.length - 1;
+        while (a <= b)
+        {
+            int i = (a + b) / 2;
+            if (array[i] < value)
+                a = i + 1;
+            else if (array[i] > value)
+                b = i - 1;
+            else
                 return true;
-            }
         }
         return false;
     }
 
     public static int[] getPrimesBetween(int a, int b, int[] array) {
-
         int[] full_prime_array = getFirstPrimes(b);
         int[] primes_between = new int[b + 1];
         int counter = 0;
@@ -116,13 +95,11 @@ class PrimeUtils {
         }
 
         return primes_between_trimmed;
-
-
     }
 
     // end point is not included
-    public static int getRandomRange(int a, int b){
-        return (int)(Math.random()*(b-a))+a;
+    public static int getRandomRange(int a, int b) {
+        return (int)(Math.random() * (b - a)) + a;
     }
 
     public static int getRandomPrimeFrom(int[] primes) {
