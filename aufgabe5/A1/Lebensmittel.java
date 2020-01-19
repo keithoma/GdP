24fb3fab3b8afbe7cda3711c681fabd35e4ff272
Christian Parpart <christian@parpart.family>
2020-01-19 23:43:31 +0100

    fixed?

--- aufgabe5/A1/Lebensmittel.java
+++ aufgabe5/A1/Lebensmittel.java
@@ -10,9 +10,9 @@ public abstract class Lebensmittel {
 		this.unit = unit;
 	}
 
-	public abstract boolean essen();
+	public abstract boolean essen(int menge);
 
-	public abstract boolean trinken();
+	public abstract boolean trinken(int menge);
 
     public String status() {
         return String.format("%s (%s, %d %s)", this.getClass().getName(), this.name, this.menge, this.unit);
