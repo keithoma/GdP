21053ef6880f3bf7839b9da68fad6349edc2461c
keithoma <thomakmj@gmail.com>
2020-02-02 03:04:25 +0100

    Selection Sort should work

--- aufgabe6/SelectionSort.java
+++ aufgabe6/SelectionSort.java
@@ -1,10 +1,10 @@
 public class SelectionSort
 {
-    int frequency(String s, char ch)
+    static int frequency(String s, char ch)
     {
         int f = 0;
-        for (char c : s)
-            if (c == ch)
+        for (int i = 0; i < s.length(); ++i)
+            if (s.charAt(i) == ch)
                 ++f;
         return f;
     }
@@ -12,40 +12,42 @@ public class SelectionSort
     // sortiere das Array a mit SelectionSort
     public static void sort(String[] a, char c)
     {
-        // for (int i = 0; i < a.length - 1; ++i) {
-        //     int fi = frequency(a[i]);
-        //     int j = i + 1;
-        //     for (; j < a.length; ++j) {
-        //         int fj = frequency(a[j]);
-        //         if (fj < )
-        //     }
-        //     if (i != j) {
-        //         String tmp = a[j];
-        //         a[j] = a[i];
-        //         a[i] = tmp;
-        //     }
-        // }
-        // TODO: selection sort
+        for (int i = 0; i < a.length - 1; i++) {
+            // int fi = frequency(a[i], c);
+            int max_idx = i;
+            for (int j = i + 1; j < a.length; j++) {
+                int fj = frequency(a[j], c);
+                int fmax = frequency(a[max_idx], c);
+                if (fj > fmax) {
+                    max_idx = j;
+                }
+            }
+            // swap
+            String tmp = a[max_idx];
+            a[max_idx] = a[i];
+            a[i] = tmp;
+        }
     }
 
     public static void main(String[] argv)
     {
-        // ueberpruefe Eingabeparameter
-        if(argv.length == 0 || argv[0].length() != 1) {
-            System.out.println("Fehler: es wird ein Buchstabe als Parameter erwartet");
-            System.exit(-1);
+        String[] poem = {
+            "Some say the world will end in fire,",
+            "Some say in ice.",
+            "From what I've tasted of desire",
+            "I hold with those who favor fire.",
+            "But if it had to perish twice,",
+            "I think I know enough of hate",
+            "To say that for destruction ice",
+            "Is also great",
+            "And would suffice."
+        };
+
+        sort(poem, 'o');
+
+        for (int i = 0; i < 9; ++i) {
+            int f = frequency(poem[i], 'o');
+            System.out.print(String.format("%d: %s\n", f, poem[i]));
         }
-
-        // den buchstaben aus dem Parameter extrahieren
-        char c = argv[0].charAt(0);
-
-        // alle Zeilen ueber die Konsole einlesen
-        String[] a = StdIn.readAllLines();
-
-        // Array sortieren
-        sort(a, c);
-
-        // TODO: sortierte Liste ausgeben
-
     }
 }
